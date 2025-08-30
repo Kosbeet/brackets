5f9f99ccc02e671e0bc66b5cc40095256fde33ea
Konstantin <beeters88@gmail.com>
2025-08-30 03:50:12 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const open = bracketsConfig.map(([key]) => key);
+  const close = Object.fromEntries(
+    bracketsConfig.map((elem) => elem.reverse())
+  );
+  const same = bracketsConfig
+    .filter(([key, value]) => key === value)
+    .map(([key]) => key);
+  const stack = [];
+  for (let i = 0; i < str.length; i += 1) {
+    const top = stack[stack.length - 1];
+    if (open.includes(str[i])) {
+      if (stack.length > 0 && same.includes(str[i]) && top === str[i]) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (close[str[i]] === top) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  console.log(stack, 'stack');
+  return stack.length === 0;
 };
